dc9ee10f0fd6a793d61121ead6ac1944f83c5ee9
Didier Carrillo <didiercarrillo40@gmail.com>
2025-11-03 11:51:56 -0600

    line 177

--- src/libscheduler/libscheduler.c
+++ src/libscheduler/libscheduler.c
@@ -164,16 +164,17 @@ int scheduler_new_job(int job_number, int time, int running_time, int priority)
       
       if(coreJobs[i] != NULL){
         int compare = 0;
+        int comparePreemptCore = 0;
         if(schedScheme == SJF){
           compare = comparerSJF(newJob, coreJobs[i]);
-          //comparePreemptCore = comparerSJF(coreJobs[i], coreJobs[preemptCore]);
+          comparePreemptCore = comparerSJF(coreJobs[i], coreJobs[preemptCore]);
         }else{
           compare = comparerPRI(newJob, coreJobs[i]);
-          //comparePreemptCore = comparerPRI(coreJobs[i], coreJobs[preemptCore]);
+          comparePreemptCore = comparerPRI(coreJobs[i], coreJobs[preemptCore]);
         }
-
+        //(schedScheme == SJF ? comparerSJF(coreJobs[i], coreJobs[preemptCore]) > 0 : comparerPRI(coreJobs[i], coreJobs[preemptCore]) > 0)
         if(compare < 0){
-          if(preemptCore == -1 || (schedScheme == SJF ? comparerSJF(coreJobs[i], coreJobs[preemptCore]) > 0 : comparerPRI(coreJobs[i], coreJobs[preemptCore]) > 0)){
+          if(preemptCore == -1 || comparePreemptCore > 0){
             preemptCore = i;
           }
         }
